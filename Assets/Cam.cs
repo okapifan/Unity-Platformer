1748a78902ad9edd65bea21190ccf7450d058459
okapifan <wxzegers@gmail.com>
2020-01-06 13:36:57 +0100

    first commit

---
+++ Assets/Cam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Cam : NetworkBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) { return; }
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+}
