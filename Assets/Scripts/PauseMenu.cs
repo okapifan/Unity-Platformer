b0365b50a391ae4273f9aef45c5a1835cf5dfe1a
okapifan <wxzegers@gmail.com>
2020-01-06 23:41:07 +0100

    Added Pause-Menu, New Tiles

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pm;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isGamePaused) {
+                resume();
+            } else {
+                pause();
+            }
+        }
+    }
+
+    public void resume() {
+        pm.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void pause() {
+        pm.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+
+    public void settings() {
+        //Todo
+    }
+}
